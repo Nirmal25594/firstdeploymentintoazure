dd58b42ee46e2f2f291f640b306736c048aaf23f
MyFirstGit <nirmalkumar25594@gmail.com>
2025-12-27 21:03:30 -0500

    Java version change

--- src/main/java/com/nirmal/FirstDeploymentApplication.java
+++ src/main/java/com/nirmal/FirstDeploymentApplication.java
@@ -1,4 +1,11 @@
 package com.nirmal;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FirstDeploymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstDeploymentApplication.class, args);
+    }
 }
